5a6dfe89382c74aa5849d73b9368e8483c212349
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-23 11:21:11 +0100

    wip

--- src/app/pages/basic-plan/basic-plan.component.ts
+++ src/app/pages/basic-plan/basic-plan.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LanguageSelectorComponent } from '../../components/language-selector/language-selector.component';
 
 @Component({
   selector: 'app-basic-plan',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LanguageSelectorComponent],
   templateUrl: './basic-plan.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
