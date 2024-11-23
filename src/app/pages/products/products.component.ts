0805c9900f4f612220ecf063ec1a95f76488b129
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-23 11:21:17 +0100

    wip

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LanguageSelectorComponent } from "../../components/language-selector/language-selector.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LanguageSelectorComponent],
   templateUrl: './products.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
