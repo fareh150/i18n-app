0c5a4544d81ee830a1b2ca9c35f0078be9961541
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-23 11:53:18 +0100

    basic inject and usage of cookies

--- src/app/components/language-selector/language-selector.component.ts
+++ src/app/components/language-selector/language-selector.component.ts
@@ -1,18 +1,32 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
 
 @Component({
   selector: 'app-language-selector',
   standalone: true,
   imports: [],
+  providers: [SsrCookieService],
   templateUrl: './language-selector.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LanguageSelectorComponent
 {
+  cookieService = inject(SsrCookieService);
+
   languages = signal([
     { code: 'en', flag: '🇺🇸' },
     { code: 'es', flag: '🇪🇸' },
     { code: 'fr', flag: '🇫🇷' },
     { code: 'it', flag: '🇮🇹' },
   ]);
+
+  changeLanguage(event: Event)
+  {
+    const target = event.target as HTMLSelectElement;
+    const value = target.value;
+    this.cookieService.set('language', target.value);
+
+    console.log('Language changed to', {value});
+
+  }
 }
