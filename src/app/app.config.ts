7d81fdbe55ee515ca0b0f1c8c95f8b5b53b0ded5
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-23 13:30:24 +0100

    [CONF] NGX Translate npm , configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideTranslateService, TranslateLoader } from "@ngx-translate/core";
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) => {
+  return new TranslateHttpLoader(http, './i18n/', '.json'); // Ruta de las traducciones
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()), // Configuración de HttpClient
+    provideTranslateService({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient], // Inyección de dependencia de HttpClient
+      },
+    }),
+  ]
 };
