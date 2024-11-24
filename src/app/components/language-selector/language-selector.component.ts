41a247b32b024d622da4b14f83baf6bee3791c04
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-24 10:39:46 +0100

    wip

--- src/app/components/language-selector/language-selector.component.ts
+++ src/app/components/language-selector/language-selector.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { SsrCookieService } from 'ngx-cookie-service-ssr';
 import { LanguageService } from '../../service/language.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-language-selector',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   providers: [SsrCookieService],
   templateUrl: './language-selector.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/basic-plan/basic-plan.component.ts
+++ src/app/pages/basic-plan/basic-plan.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LanguageSelectorComponent } from '../../components/language-selector/language-selector.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-basic-plan',
   standalone: true,
-  imports: [RouterLink, LanguageSelectorComponent],
+  imports: [RouterLink, LanguageSelectorComponent, TranslatePipe],
   templateUrl: './basic-plan.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
