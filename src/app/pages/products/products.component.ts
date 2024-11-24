e235f7a86755b6476500edf0fe983ff34b7ee624
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-24 10:12:14 +0100

    [Translate] usage

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LanguageSelectorComponent } from "../../components/language-selector/language-selector.component";
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterLink, LanguageSelectorComponent],
+  imports: [
+    RouterLink,
+    LanguageSelectorComponent,
+    TranslatePipe
+  ],
   templateUrl: './products.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
